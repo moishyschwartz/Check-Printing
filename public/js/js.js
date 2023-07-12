8bba956b209ee43ada3d33fa32556ecc4a5c4273
moishy Schwartz <moishy8597@gmail.com>
2023-07-11 20:53:32 -0400

    Added totals and some css

--- public/js/js.js
+++ public/js/js.js
@@ -1,83 +1,64 @@
-const singleForm = document.querySelector('#ind')
-const singleButton = document.querySelector('.ind')
-const importForm = document.querySelector('#import')
-const importButton = document.querySelector('.imp')
-const form = document.querySelector('form')
-
-// document.addEventListener("visibilitychange", function() {
-//     if (document.hidden){
-//         location.reload()
-//     }
-// });
+const singleForm = document.querySelector("#ind");
+const singleButton = document.querySelector(".ind");
+const importForm = document.querySelector("#import");
+const importButton = document.querySelector(".imp");
+const form = document.querySelector("form");
 
 const ind = () => {
-    singleForm.style.display = 'block'
-    singleButton.style.background = '#333'
-    singleButton.style.color = '#fff'
-    importForm.style.display = 'none'
-    importButton.style.background = '#eee'
-    importButton.style.color = '#333'
-     
-}
+  singleForm.style.display = "block";
+  singleButton.style.background = "#333";
+  singleButton.style.color = "#fff";
+  importForm.style.display = "none";
+  importButton.style.background = "#eee";
+  importButton.style.color = "#333";
+};
 
 const imp = () => {
-    singleForm.style.display = 'none'
-    singleButton.style.background = '#eee'
-    singleButton.style.color = '#333'
-    importForm.style.display = 'block'
-    importButton.style.background = '#333'
-    importButton.style.color = '#fff'
-     
-}
+  singleForm.style.display = "none";
+  singleButton.style.background = "#eee";
+  singleButton.style.color = "#333";
+  importForm.style.display = "block";
+  importButton.style.background = "#333";
+  importButton.style.color = "#fff";
+};
 
 const report = () => {
-    const startDate = document.querySelector('#startDate').value
-    const endDate = document.querySelector('#endDate').value
-    window.open(`/report?startDate=${startDate}&endDate=${endDate}`);
-}
+  const startDate = document.querySelector("#startDate").value;
+  const endDate = document.querySelector("#endDate").value;
+  window.open(`/report?startDate=${startDate}&endDate=${endDate}`);
+};
 
 const checkData = () => {
-    const startDate = document.querySelector('#startDate').value
-    const endDate = document.querySelector('#endDate').value
-    const formitug = document.querySelector('#formitug')
-    const nuchmitug = document.querySelector('#Nuchmitug')
-    const amount = document.querySelector('#amount')
-    const totalFormitug = 0
-    const totalNuchmitug = 0
-    const totalAmount = 0
+  document.querySelector("#data").style.display = "none";
+  const startDate = document.querySelector("#startDate").value;
+  const endDate = document.querySelector("#endDate").value;
+  const formitug = document.querySelector("#formitug");
+  const nuchmitug = document.querySelector("#Nuchmitug");
+  const amount = document.querySelector("#totalAmount");
+  let totalFormitug = 0;
+  let totalNuchmitug = 0;
+  let totalAmount = 0;
 
-    fetch(`https://kollelsys.com/api/half/hour/61441012/${startDate}/${endDate}`).then((respons) => {
-        respons.json().then((data) => {
-            if (data.success == false ) {return}
+  fetch(
+    `https://kollelsys.com/api/half/hour/61441012/${startDate}/${endDate}`
+  ).then((respons) => {
+    respons.json().then((data) => {
+      if (data.success == false) {
+        return;
+      }
 
-            data.forEach(element => {
-                totalFormitug += Number(element.TimesMorning)
-                totalNuchmitug += Number(element.TimesAfternoon)
-            });
+      data.half_hour.forEach((element) => {
+        totalFormitug += Number(element.TimesMorning);
+        totalNuchmitug += Number(element.TimesAfternoon);
+      });
 
-            document.querySelector('#data').style.display = 'block'
-            formitug.innerHTML = totalFormitug
-            nuchmitug.innerHTML = totalNuchmitug
-
-
-
-        })
-    })
-
-}
-
-// importForm.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     const startDate = document.querySelector('#startDate').value
-//     const endDate = document.querySelector('#endDate').value
-//     const report = document.querySelector('#report')
-
-//     fetch('https://kollelsys.com/admin/reports/half/load?date_from='+ startDate +'&date_to=' + endDate).then((respons) => {
-//         respons.json().then((data) => {
-//             report.innerHtml = data
-
-//         })
-//     })
-// })
-
+      document.querySelector("#data").style.display = "block";
+      checks.innerHTML = " " + data.half_hour.length;
+      const total = totalFormitug * 20 + totalNuchmitug * 10;
+      amount.innerHTML = " " + total.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    });
+  });
+};
